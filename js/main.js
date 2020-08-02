c4222688b23e642bc79dcf5ae30f954d4e92cb3b
Ryan Fuhrman <rsfuhrman10@gmail.com>
2020-08-02 18:10:09 -0400

    Set up GIPHY API call for trending gifs and added to .trending-gif-list on load

--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
-async function getTrending(url) {
-  const response = await fetch(url);
-  console.log(response);
-  // return response.json();
-}
+const view = {
+  populateTrending: function (data) {
+    data.data.map((gif) => {
+      return (trendingList.innerHTML += `<li id="${gif.id}">
+          <img src="${gif.images.original.url}" alt="${gif.title}"></img>
+        </li>`);
+    });
+  },
+};
 
-getTrending("api.giphy.com/v1/gifs/trending").then((data) => console.log(data));
+const controller = {
+  starterFunction: function () {
+    this.getTrending(
+      `http://api.giphy.com/v1/gifs/trending?api_key=${config.API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        view.populateTrending(data);
+        console.log(data);
+      });
+  },
+  getTrending: async function (url) {
+    const response = await fetch(url);
+    return response;
+  },
+};
+
+const model = {};
+
+const trendingList = document.querySelector(".trending-gifs-list");
+
+controller.starterFunction();
