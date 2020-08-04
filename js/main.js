f5b6383847de411fa8dc968d5d7f37378a171b5a
Ryan Fuhrman <rsfuhrman10@gmail.com>
2020-08-04 14:38:05 -0400

    Added 'Load More' button functionality and search when enter button is pressed

--- js/main.js
+++ js/main.js
@@ -4,13 +4,17 @@
 
 const gifsList = document.querySelector(".gifs-list");
 const gifsTitle = document.querySelector(".gifs-title");
-const search = document.querySelector(".gif-search");
 const trendingButton = document.querySelector(".trending-button");
+const searchField = document.querySelector(".gif-search-input");
 const searchButton = document.querySelector(".gif-search-button");
+const loadMoreButton = document.querySelector(".load-more-button");
 const limit = 10;
+let offset = 0;
+let lastSearch = "";
 
 const view = {
   populateTrending: function (data) {
+    offset = 0;
     gifsList.innerHTML = "";
     gifsTitle.innerHTML = "Trending GIFs";
     data.data.map((gif) => {
@@ -19,8 +23,12 @@ const view = {
         </li>`);
     });
   },
-  populateSearch: function (data, searchQuery) {
-    gifsList.innerHTML = "";
+  populateSearch: function (data, searchQuery, clearResults) {
+    searchField.value = "";
+    if (clearResults) {
+      gifsList.innerHTML = "";
+      offset = 0;
+    }
     gifsTitle.innerHTML = `Results for "${searchQuery}"`;
     data.data.map((gif) => {
       return (gifsList.innerHTML += `<li id="${gif.id}">
@@ -32,11 +40,14 @@ const view = {
 
 const controller = {
   starterFunction: function () {
-    this.trendingGifs();
-    this.searchInput();
     this.getTrending();
+    this.searchInput();
+    this.getTrendingByButton();
+
+    loadMoreButton.addEventListener("click", this.loadMore);
   },
-  trendingGifs: function () {
+  getTrending: function () {
+    loadMoreButton.style.display = "none";
     this.getGifs(
       `http://api.giphy.com/v1/gifs/trending?api_key=${config.API_KEY}&limit=${limit}`
     )
@@ -49,20 +60,39 @@ const controller = {
     const response = await fetch(url);
     return response;
   },
-  getTrending: function () {
-    trendingButton.addEventListener("click", () => this.trendingGifs());
+  getTrendingByButton: function () {
+    trendingButton.addEventListener("click", () => this.getTrending());
+  },
+  getSearch: function () {
+    loadMoreButton.style.display = "block";
+    const searchQuery = searchField.value;
+    lastSearch = searchQuery;
+    this.getGifs(
+      `http://api.giphy.com/v1/gifs/search?q=${searchQuery}&api_key=${config.API_KEY}&limit=${limit}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        view.populateSearch(data, searchQuery, true);
+      });
   },
   searchInput: function () {
-    searchButton.addEventListener("click", () => {
-      const searchQuery = search.value;
-      this.getGifs(
-        `http://api.giphy.com/v1/gifs/search?q=${searchQuery}&api_key=${config.API_KEY}&limit=${limit}`
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          view.populateSearch(data, searchQuery);
-        });
+    searchField.addEventListener("keyup", (e) => {
+      if (e.keyCode === 13) {
+        this.getSearch();
+      }
     });
+    searchButton.addEventListener("click", this.getSearch);
+  },
+  loadMore: function () {
+    offset += 10;
+    controller
+      .getGifs(
+        `http://api.giphy.com/v1/gifs/search?q=${lastSearch}&api_key=${config.API_KEY}&limit=${limit}&offset=${offset}`
+      )
+      .then((response) => response.json())
+      .then((data) => {
+        view.populateSearch(data, lastSearch, false);
+      });
   },
 };
 
